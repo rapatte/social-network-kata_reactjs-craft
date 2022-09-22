0987353a74d8778349bc294b0db8f06873bd9c4b
rapatte <agayoso@wemanity.com>
2022-09-22 15:25:38 +0200

    feat: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+import { Home, Timeline } from "./component";
+import { PAGE_HOME, PAGE_TIMELINE } from "./constants/router";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path={PAGE_HOME} element={<Home />} />
+        <Route path={PAGE_TIMELINE} element={<Timeline />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/constants/router.ts
@@ -0,0 +1,13 @@
+export const PAGE_HOME = '/home';
+export const PAGE_TIMELINE = '/timeline';
+
+export const routes = [
+    {
+        name: "Home",
+        path: PAGE_HOME,
+    },
+    {
+        name: "Timeline",
+        path: PAGE_TIMELINE,
+    }
+];
